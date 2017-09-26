ed85a30c7a10837b1a4d86a727f84a2bc469e070
Kai Wells <support@kaiwells.me>
2017-09-26 14:03:38 -0700

    Strip it all away

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-var express = require('express');
+var express = require("express");
+var url = require("url");
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set("port", (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.get("/v1", function(request, response) {
+    response.json({message: "hello"})
+});
 
-// views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-//
-// app.get('/', function(request, response) {
-//   response.render('pages/index');
-// });
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get("port"), function() {
+    console.log("Node app is running on port", app.get("port"));
 });
