2feb70f2e5be26326e826f7b97bb02807943716d
Kai Wells <support@kaiwells.me>
2017-09-26 14:18:18 -0700

    Testing pg

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ var express = require("express");
 var url = require("url");
 var app = express();
 
+const { Client } = require('pg');
+const client = new Client();
+
 app.set("port", (process.env.PORT || 5000));
 
 app.get("/v1", function(request, response) {
-    response.json({message: "hello"})
+    var query = url.parse(request.url, true).query;
+
+    client.connect();
+    client.query("SELECT * FROM scorecard WHERE state = 'CA'")
+        .then(res => console.log(res.rows[0]))
+        .catch(e => console.log(e.stack));
+
+    response.json({message: "hello"});
 });
 
 app.listen(app.get("port"), function() {
